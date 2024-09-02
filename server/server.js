710797a12ecf7ba5aaaa7c8c4f11cbcda0cc4eca
Ajas Sharafudeen <ajas.sharaf95@gmail.com>
2024-09-02 13:40:00 +0530

    cors configured, cookieParser and express.json used,server listening on PORT 5000

--- server/server.js
+++ server/server.js
@@ -1,12 +1,35 @@
-const express = require('express')
-const mongoose = require('mongoose')
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
-require('dotenv').config()
+require("dotenv").config();
 
-mongoose.connect(process.env.MONGODB_URL)
-.then(()=> console.log('MongoDB connected'))
-.catch(err => console.log(err));
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
 
+const app = express();
+const PORT = process.env.PORT || 5000;
 
-const app = express()
-const PORT = process.env.PORT || 5000
+//cors configuration
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Cache-Control",
+      "Expires",
+      "Pragma",
+    ],
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
