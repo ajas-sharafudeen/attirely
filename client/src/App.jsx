0895b9d52a6b0e5099b81cdd67e1e0518f6b5695
Ajas Sharafudeen <ajas.sharaf95@gmail.com>
2024-09-02 17:28:06 +0530

    added all routes related to Admin and Shopping. Also added route for pages that doesn't exist.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,41 @@
-import { Routes, Route } from "react-router-dom"
-import AuthLayout from "./components/auth/layout"
-import AuthLogin from "./pages/auth/login"
-import AuthRegister from "./pages/auth/register"
+import { Routes, Route } from 'react-router-dom'
+import AuthLayout from './components/auth/layout'
+import AuthLogin from './pages/auth/login'
+import AuthRegister from './pages/auth/register'
+import AdminLayout from './components/admin-view/layout'
+import AdminDashboard from './pages/admin-view/dashboard'
+import AdminFeatures from './pages/admin-view/features'
+import AdminOrders from './pages/admin-view/orders'
+import AdminProducts from './pages/admin-view/products'
+import NotFound from './pages/not-found'
+import ShoppingLayout from './components/shopping-view/layout'
+import ShoppingAccount from './pages/shopping-view/account'
+import ShoppingCheckout from './pages/shopping-view/checkout'
+import ShoppingHome from './pages/shopping-view/home'
+import ShoppingListing from './pages/shopping-view/listing'
 
 function App() {
 
   return (
-    <div className="flex flex-col overflow-hidden bg-white">
-      <h1>Header component</h1>
+    <div className='flex flex-col overflow-hidden bg-white'>
       <Routes>
-        <Route path="/auth" element={<AuthLayout />}>
-          <Route path="login" element={<AuthLogin />}/>
-          <Route path="register" element={<AuthRegister />}/>
+        <Route path='/auth' element={<AuthLayout />}>
+          <Route path='login' element={<AuthLogin />} />
+          <Route path='register' element={<AuthRegister />} />
         </Route>
+        <Route path='/admin' element={<AdminLayout />}>
+          <Route path='dashboard' element={<AdminDashboard />} />
+          <Route path='features' element={<AdminFeatures />} />
+          <Route path='orders' element={<AdminOrders />} />
+          <Route path='products' element={<AdminProducts />} />
+        </Route>
+        <Route path='/shop' element={<ShoppingLayout />}>
+          <Route path='account' element={<ShoppingAccount />} />
+          <Route path='checkout' element={<ShoppingCheckout />} />
+          <Route path='home' element={<ShoppingHome />} />
+          <Route path='listing' element={<ShoppingListing />} />
+        </Route>
+        <Route path='*' element={<NotFound />}></Route>
       </Routes>
     </div>
   )
