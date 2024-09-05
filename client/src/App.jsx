0758900899b6f02d3607d11a680be37128b43eee
Ajas Sharafudeen <ajas.sharaf95@gmail.com>
2024-09-05 19:48:30 +0530

    dummy state reverted to default values

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -18,11 +18,8 @@ import UnauthPage from './pages/unauth-page'
 
 function App() {
   // dummy state
-  const isAuthenticated = true
-  const user = {
-    name: 'ajas',
-    role: 'admin'
-  }
+  const isAuthenticated = false
+  const user = null
 
   return (
     <div className='flex flex-col overflow-hidden bg-white'>
