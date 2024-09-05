9a60fb6ffa56dc7014fca59a89bc4fcac73b4007
Ajas Sharafudeen <ajas.sharaf95@gmail.com>
2024-09-05 19:49:03 +0530

    register component in progress...

--- client/src/pages/auth/register.jsx
+++ client/src/pages/auth/register.jsx
@@ -1,7 +1,12 @@
 export default function AuthRegister() {
   return (
-    <div>
-      Register
+    <div className="mx-auto w-full max-w-md space-y-6">
+      <div className="text-center">
+        <h1 className="text-3xl font-bold tracking-tight text-foreground">Create a new account</h1>
+        <p>Already have an account
+
+        </p>
+      </div>
     </div>
   )
 }
