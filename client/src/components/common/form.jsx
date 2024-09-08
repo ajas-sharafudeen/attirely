c5e6d8aaf519ba8c66cd67d7b1b0bdb08718b3e4
Ajas Sharafudeen <ajas.sharaf95@gmail.com>
2024-09-08 11:08:01 +0530

    bug: added button ui

--- client/src/components/common/form.jsx
+++ client/src/components/common/form.jsx
@@ -1,8 +1,13 @@
-import { SelectContent, SelectItem, SelectTrigger, SelectValue } from "@radix-ui/react-select";
+import {
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue
+} from "../ui/select";
 import { Input } from "../ui/input";
 import { Label } from "../ui/label";
-import { Select } from "../ui/select";
 import { Textarea } from "../ui/textarea";
+import { Button } from "../ui/button";
 
 const types = {
   INPUT: 'input',
@@ -14,7 +19,6 @@ export default function CommonForm({ formControls, formData, setFormData, onSubm
 
   function renderInputsByComponentType(getControlItem) {
     let element = null
-
     const value = formData[getControlItem.name] || ''
 
     switch (getControlItem.componentType) {
@@ -36,18 +40,26 @@ export default function CommonForm({ formControls, formData, setFormData, onSubm
         break;
       case types.SELECT:
         element = (
-          <Select onValueChange={(value) => setFormData({
-            ...formData,
-            [getControlItem.name]: value
-          })} value={value}>
+          <Select
+            onValueChange={(value) =>
+              setFormData({
+                ...formData,
+                [getControlItem.name]: value
+              })
+            }
+            value={value}
+          >
             <SelectTrigger className='w-full'>
               <SelectValue placeholder={getControlItem.placeholder} />
             </SelectTrigger>
             <SelectContent>
               {
-                getControlItem.options &&
-                  getControlItem.options.length > 0 ?
-                  getControlItem.options.map(optionItem => <SelectItem key={optionItem.id} value={optionItem.id}>{optionItem.label}</SelectItem>) : null
+                getControlItem.options && getControlItem.options.length > 0
+                  ? getControlItem.options.map(optionItem =>
+                    <SelectItem key={optionItem.id} value={optionItem.id}>
+                      {optionItem.label}
+                    </SelectItem>)
+                  : null
               }
             </SelectContent>
           </Select>
@@ -61,23 +73,33 @@ export default function CommonForm({ formControls, formData, setFormData, onSubm
             placeholder={getControlItem.placeholder}
             id={getControlItem.name}
             value={value}
+            onChange={event =>
+              setFormData({
+                ...formData,
+                [getControlItem.name]: event.target.value
+              })
+            }
           />
         )
 
         break;
 
       default:
-        element = <Input
-          name={getControlItem.name}
-          placeholder={getControlItem.placeholder}
-          id={getControlItem.name}
-          type={getControlItem.type}
-          value={value}
-          onChange={event => setFormData({
-            ...formData,
-            [getControlItem.name]: event.target.value
-          })}
-        />
+        element = (
+          <Input
+            name={getControlItem.name}
+            placeholder={getControlItem.placeholder}
+            id={getControlItem.name}
+            type={getControlItem.type}
+            value={value}
+            onChange={event =>
+              setFormData({
+                ...formData,
+                [getControlItem.name]: event.target.value
+              })
+            }
+          />
+        )
         break;
     }
     return element
@@ -94,7 +116,9 @@ export default function CommonForm({ formControls, formData, setFormData, onSubm
             </div>
           ))}
       </div>
-      <Button type='submit' className='mt-2 w-full'>{buttonText || 'Submit'}</Button>
+      <Button type='submit' className='mt-2 w-full'>
+        {buttonText || 'Submit'}
+      </Button>
     </form>
   )
 }
