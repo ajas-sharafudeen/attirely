4fb0f7ffe806aebc390d7156b1cd7bca2eaf4dea
Ajas Sharafudeen <ajas.sharaf95@gmail.com>
2024-09-09 20:43:54 +0530

    aligned code

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,40 +1,40 @@
 export const registerFormControls = [
   {
-    name:'userName',
-    label:'User Name',
+    name: 'userName',
+    label: 'User Name',
     placeholder: 'Enter your user name',
     componentType: 'input',
-    type:'text'
+    type: 'text',
   },
   {
-    name:'email',
-    label:'Email',
+    name: 'email',
+    label: 'Email',
     placeholder: 'Enter your email',
     componentType: 'input',
-    type:'email'
+    type: 'email',
   },
   {
-    name:'password',
-    label:'Password',
+    name: 'password',
+    label: 'Password',
     placeholder: 'Enter your password',
     componentType: 'input',
-    type:'password'
+    type: 'password',
   },
-]
+];
 
 export const loginFormControls = [
   {
-    name:'email',
-    label:'Email',
+    name: 'email',
+    label: 'Email',
     placeholder: 'Enter your email',
     componentType: 'input',
-    type:'email'
+    type: 'email',
   },
   {
-    name:'password',
-    label:'Password',
+    name: 'password',
+    label: 'Password',
     placeholder: 'Enter your password',
     componentType: 'input',
-    type:'password'
+    type: 'password',
   },
-]
+];
