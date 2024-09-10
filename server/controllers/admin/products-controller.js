44c2fab8ca669df08d8f19c265876b38674f094b
Ajas Sharafudeen <ajas.sharaf95@gmail.com>
2024-09-10 18:38:39 +0530

    controller for fetching,adding,updating and deleting product

--- server/controllers/admin/products-controller.js
+++ server/controllers/admin/products-controller.js
@@ -1,4 +1,5 @@
 const { imageUploadUtil } = require('../../helpers/cloudinary');
+const Product = require('../../models/Product');
 
 const handleImageUpload = async (req, res) => {
   try {
@@ -19,4 +20,132 @@ const handleImageUpload = async (req, res) => {
   }
 };
 
-module.exports = { handleImageUpload };
+//add a new product
+const addProduct = async (req, res) => {
+  try {
+    const {
+      image,
+      title,
+      description,
+      category,
+      brand,
+      price,
+      salePrice,
+      totalStock,
+    } = req.body;
+    const newlyCreatedProduct = new Product({
+      image,
+      title,
+      description,
+      category,
+      brand,
+      price,
+      salePrice,
+      totalStock,
+    });
+
+    await newlyCreatedProduct.save();
+    res.status(201).json({
+      success: true,
+      data: newlyCreatedProduct,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: 'Error occured',
+    });
+  }
+};
+//fetch all products
+const fetchAllProducts = async (req, res) => {
+  try {
+    const listOfProducts = await Product.find({});
+    res.status(200).json({
+      success: true,
+      message: listOfProducts,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: 'Error occured',
+    });
+  }
+};
+//edit a product
+const editProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const {
+      image,
+      title,
+      description,
+      category,
+      brand,
+      price,
+      salePrice,
+      totalStock,
+    } = req.body;
+
+    const findProduct = await Product.findById(id);
+
+    if (!findProduct)
+      return res.status(404).json({
+        success: false,
+        message: 'Product not found',
+      });
+
+    findProduct.title = title || findProduct.title;
+    findProduct.description = description || findProduct.description;
+    findProduct.category = category || findProduct.category;
+    findProduct.brand = brand || findProduct.brand;
+    findProduct.price = price || findProduct.price;
+    findProduct.salePrice = salePrice || findProduct.salePrice;
+    findProduct.totalStock = totalStock || findProduct.totalStock;
+    findProduct.image = image || findProduct.image;
+
+    await findProduct.save();
+    res.status(200).json({
+      success: true,
+      data: findProduct,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: 'Error occured',
+    });
+  }
+};
+//delete a product
+const deleteProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndDelete(id);
+
+    if (!product)
+      return res.status(404).json({
+        success: false,
+        message: 'Product not found',
+      });
+
+    res.status(200).json({
+      success: true,
+      message: 'Product is deleted successfully',
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: 'Error occured',
+    });
+  }
+};
+module.exports = {
+  handleImageUpload,
+  addProduct,
+  fetchAllProducts,
+  editProduct,
+  deleteProduct,
+};
