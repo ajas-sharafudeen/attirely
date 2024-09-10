3e6a1ad3f449cdb2e6ba77d591f0d918964408f0
Ajas Sharafudeen <ajas.sharaf95@gmail.com>
2024-09-10 17:42:06 +0530

    adminProductsRouter route is now live

--- server/server.js
+++ server/server.js
@@ -1,15 +1,16 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const cookieParser = require("cookie-parser");
-const cors = require("cors");
-const authRouter = require("./routes/auth/auth-routes");
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const authRouter = require('./routes/auth/auth-routes');
+const adminProductsRouter = require('./routes/admin/products-routes');
 
-require("dotenv").config();
+require('dotenv').config();
 
 // Creates a database connection
 mongoose
   .connect(process.env.MONGODB_URL)
-  .then(() => console.log("MongoDB connected"))
+  .then(() => console.log('MongoDB connected'))
   .catch((err) => console.log(err));
 
 const app = express();
@@ -18,14 +19,14 @@ const PORT = process.env.PORT || 5000;
 //cors configuration
 app.use(
   cors({
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST", "DELETE", "PUT"],
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'DELETE', 'PUT'],
     allowedHeaders: [
-      "Content-Type",
-      "Authorization",
-      "Cache-Control",
-      "Expires",
-      "Pragma",
+      'Content-Type',
+      'Authorization',
+      'Cache-Control',
+      'Expires',
+      'Pragma',
     ],
     credentials: true,
   })
@@ -33,6 +34,7 @@ app.use(
 
 app.use(cookieParser());
 app.use(express.json());
-app.use("/api/auth", authRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/admin/products', adminProductsRouter);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
