c0852af4c2e7be1b7e9e51b71021a1168579ea00
Ajas Sharafudeen <ajas.sharaf95@gmail.com>
2024-09-11 16:05:58 +0530

    products can now be added

--- client/src/pages/admin-view/products.jsx
+++ client/src/pages/admin-view/products.jsx
@@ -3,7 +3,10 @@ import CommonForm from "@/components/common/form";
 import { Button } from "@/components/ui/button";
 import { Sheet, SheetContent, SheetHeader, SheetTitle } from "@/components/ui/sheet";
 import { addProductFormElements } from "@/config";
-import { Fragment, useState } from "react";
+import { useToast } from "@/hooks/use-toast";
+import { addNewProduct, fetchAllProducts } from "@/store/admin/products-slice";
+import { Fragment, useEffect, useState } from "react";
+import { useDispatch, useSelector } from 'react-redux';
 
 const initialFormData = {
   image: null,
@@ -23,9 +26,36 @@ export default function AdminProducts() {
   const [imageFile, setImageFile] = useState(null)
   const [uploadedImageUrl, setUploadedImageUrl] = useState('')
   const [imageLoadingState, setImageLoadingState] = useState(false)
+  const { productList } = useSelector((state) => state.adminProducts)
+  const dispatch = useDispatch()
+  const { toast } = useToast()
 
-  function onSubmit() {}
-  
+  function onSubmit(event) {
+    event.preventDefault()
+
+    dispatch(addNewProduct({
+      ...formData,
+      image: uploadedImageUrl
+    })
+    ).then((data) => {
+      if (data?.payload?.success) {
+        dispatch(fetchAllProducts())
+        setOpenCreateProductsDialog(false)
+        setImageFile(null)
+        setFormData(initialFormData)
+        toast({
+          title: 'Product added successfully'
+        })
+      }
+    })
+  }
+
+  useEffect(() => {
+    dispatch(fetchAllProducts())
+  }, [dispatch])
+
+  console.log(productList, uploadedImageUrl, 'productList')
+
   return (
     <Fragment>
       <div className="mb-4 w-full flex justify-end">
@@ -46,6 +76,7 @@ export default function AdminProducts() {
             uploadedImageUrl={uploadedImageUrl}
             setUploadedImageUrl={setUploadedImageUrl}
             setImageLoadingState={setImageLoadingState}
+            imageLoadingState={imageLoadingState}
           />
           <div className="py-6">
             <CommonForm
