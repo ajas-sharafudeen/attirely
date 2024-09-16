1bdac74e1d8bd2452afe56c05d41e492e7a378ae
Ajas Sharafudeen <ajas.sharaf95@gmail.com>
2024-09-16 09:13:39 +0530

    product can be deleted now

--- client/src/components/admin-view/product-tile.jsx
+++ client/src/components/admin-view/product-tile.jsx
@@ -5,10 +5,11 @@ export default function AdminProductTile({
   product,
   setFormData,
   setOpenCreateProductsDialog,
-  setCurrentEditedId
+  setCurrentEditedId,
+  handleDelete
 }) {
   return (
-    <Card className='w-full max-w-sm mx-auto'>
+    <Card className="w-full max-w-sm mx-auto">
       <div>
         <div className="relative">
           <img
@@ -20,21 +21,30 @@ export default function AdminProductTile({
         <CardContent>
           <h2 className="text-xl font-bold mb-2 mt-2">{product?.title}</h2>
           <div className="flex justify-between items-center mb-2">
-            <span className={`${product?.salePrice > 0 ? 'line-through' : ''}text-lg font-semibold text-primary`}>${product?.price}</span>
-            {
-              product?.salePrice > 0 ? <span className="text-lg font-bold">${product?.salePrice}</span> : null
-            }
+            <span
+              className={`${product?.salePrice > 0 ? "line-through" : ""
+                } text-lg font-semibold text-primary`}
+            >
+              ${product?.price}
+            </span>
+            {product?.salePrice > 0 ? (
+              <span className="text-lg font-bold">${product?.salePrice}</span>
+            ) : null}
           </div>
         </CardContent>
-        <CardFooter className='flex justify-between items-center'>
-          <Button onClick={() => {
-            setOpenCreateProductsDialog(true)
-            setCurrentEditedId(product?._id)
-            setFormData(product)
-          }}>Edit</Button>
-          <Button>Delete</Button>
+        <CardFooter className="flex justify-between items-center">
+          <Button
+            onClick={() => {
+              setOpenCreateProductsDialog(true);
+              setCurrentEditedId(product?._id);
+              setFormData(product);
+            }}
+          >
+            Edit
+          </Button>
+          <Button onClick={() => handleDelete(product?._id)}>Delete</Button>
         </CardFooter>
       </div>
     </Card>
-  )
+  );
 }
