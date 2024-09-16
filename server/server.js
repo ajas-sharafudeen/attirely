b7f889d090f3f6a4e7d80cd1b479522ba34274c8
Ajas Sharafudeen <ajas.sharaf95@gmail.com>
2024-09-16 15:45:31 +0530

    added route for products for users

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const authRouter = require('./routes/auth/auth-routes');
 const adminProductsRouter = require('./routes/admin/products-routes');
-
+const shopProductsRouter = require('./routes/shop/products-routes')
 require('dotenv').config();
 
 // Creates a database connection
@@ -36,5 +36,6 @@ app.use(cookieParser());
 app.use(express.json());
 app.use('/api/auth', authRouter);
 app.use('/api/admin/products', adminProductsRouter);
+app.use('/api/shop/products', shopProductsRouter);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
