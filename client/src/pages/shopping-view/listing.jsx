fce99667eae7dff06b53cdab4095702e283208f5
Ajas Sharafudeen <ajas.sharaf95@gmail.com>
2024-09-16 15:42:28 +0530

    listing page for shopping view

--- client/src/pages/shopping-view/listing.jsx
+++ client/src/pages/shopping-view/listing.jsx
@@ -1,5 +1,50 @@
+import ProductFilter from "@/components/shopping-view/filter";
+import { Button } from "@/components/ui/button";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuRadioItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { sortOptions } from "@/config";
+import { fetchAllProducts } from "@/store/admin/products-slice";
+import { ArrowUpDownIcon } from "lucide-react";
+import { useEffect } from "react";
+import { useDispatch } from 'react-redux';
+
 export default function ShoppingListing() {
-  return(
-    <div>Shopping view listing</div>
+  const dispatch = useDispatch()
+  //fetch list of products
+  useEffect(() => {
+    dispatch(fetchAllProducts())
+  }, [])
+
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-[300px_1fr] gap-6 p-4 md:p-6">
+      <ProductFilter />
+      <div className="bg-background w-full rounded-lg shadow-sm">
+        <div className="p-4 border-b flex items-center justify-between">
+          <h2 className="text-lg font-extrabold">All products</h2>
+          <div className="flex items-center gap-3">
+            <span className="text-muted-foreground">10 products</span>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant='outline' size='sm' className='flex items-center gap-1'>
+                  <ArrowUpDownIcon className="h-4 w-4" />
+                  <span>Sort by</span>
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align='end' className='w-[200px]'>
+                {
+                  sortOptions.map(sortItem => (
+                    <DropdownMenuRadioItem key={sortItem.id}>
+                      {sortItem.label}
+                    </DropdownMenuRadioItem>
+                  ))
+                }
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+        </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+
+        </div>
+      </div>
+    </div>
   )
 }
