cd917069faa9ddfef403eec1adebe0880366145c
Ajas Sharafudeen <ajas.sharaf95@gmail.com>
2024-09-17 21:34:37 +0530

    Implemented useSearchParams from react dom

--- client/src/pages/shopping-view/listing.jsx
+++ client/src/pages/shopping-view/listing.jsx
@@ -1,4 +1,5 @@
 import ProductFilter from "@/components/shopping-view/filter";
+import ProductDetailsDialog from "@/components/shopping-view/product-details";
 import ShoppingProductTile from "@/components/shopping-view/product-tile";
 import { Button } from "@/components/ui/button";
 import {
@@ -9,16 +10,35 @@ import {
   DropdownMenuTrigger
 } from "@/components/ui/dropdown-menu";
 import { sortOptions } from "@/config";
-import { fetchAllFilteredProducts } from "@/store/shop/products-slice";
+import { fetchAllFilteredProducts, fetchProductDetails } from "@/store/shop/products-slice";
 import { ArrowUpDownIcon } from "lucide-react";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from 'react-redux';
+import { useSearchParams } from "react-router-dom";
+
+function createSearchParamsHelper(filterParams) {
+  const queryParams = [];
+
+  for (const [key, value] of Object.entries(filterParams)) {
+    if (Array.isArray(value) && value.length > 0) {
+      const paramValue = value.join(",");
+
+      queryParams.push(`${key}=${encodeURIComponent(paramValue)}`);
+    }
+  }
+
+  console.log(queryParams, "queryParams");
+
+  return queryParams.join("&");
+}
 
 export default function ShoppingListing() {
   const dispatch = useDispatch()
-  const { productList } = useSelector(state => state.shopProducts)
+  const { productList, productDetails } = useSelector(state => state.shopProducts)
   const [filters, setFilters] = useState({})
   const [sort, setSort] = useState(null)
+  const [searchParams, setSearchParams] = useSearchParams()
+  const [openDetailsDialog, setOpenDetailsDialog] = useState(false)
 
   function handleSort(value) {
     setSort(value)
@@ -41,18 +61,39 @@ export default function ShoppingListing() {
       else copyFilters[getSectionId].splice(indexOfCurrentOption, 1)
     }
     setFilters(copyFilters)
+    sessionStorage.setItem('filters', JSON.stringify(copyFilters))
+  }
+
+  function handleGetProductDetails(getCurrentProductId) {
+    dispatch(fetchProductDetails(getCurrentProductId));
   }
 
   useEffect(() => {
-    dispatch(fetchAllFilteredProducts())
-  }, [dispatch])
+    setSort('price-lowtohigh')
+    setFilters(JSON.parse(sessionStorage.getItem('filters')) || {})
+  }, [])
 
-  console.log(filters, 'filters');
+  useEffect(() => {
+    if (filters && Object.keys(filters).length > 0) {
+      const createQueryString = createSearchParamsHelper(filters);
+      setSearchParams(new URLSearchParams(createQueryString));
+    }
+  }, [filters]);
+
+  useEffect(() => {
+    if (filters !== null && sort !== null)
+      dispatch(fetchAllFilteredProducts({ filterParams: filters, sortParams: sort }))
+  }, [dispatch, sort, filters])
+
+  useEffect(() => {
+    if (productDetails !== null) setOpenDetailsDialog(true)
+  }, [productDetails])
 
 
+  console.log(productDetails, 'productDetails');
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-[300px_1fr] gap-6 p-4 md:p-6">
+    <div className="grid grid-cols-1 md:grid-cols-[200px_1fr] gap-6 p-4 md:p-6">
       <ProductFilter filters={filters} handleFilter={handleFilter} />
       <div className="bg-background w-full rounded-lg shadow-sm">
         <div className="p-4 border-b flex items-center justify-between">
@@ -87,11 +128,12 @@ export default function ShoppingListing() {
           {
             productList && productList.length > 0 ?
               productList.map(productItem => (
-                <ShoppingProductTile product={productItem} />
+                <ShoppingProductTile handleGetProductDetails={handleGetProductDetails} product={productItem} />
               )) : null
           }
         </div>
       </div>
+      <ProductDetailsDialog open={openDetailsDialog} setOpen={setOpenDetailsDialog} productDetails={productDetails} />
     </div>
   )
 }
