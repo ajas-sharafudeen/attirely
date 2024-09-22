d9ca8f21c9e3dc500e4ad96306c599c29781b4d0
Ajas Sharafudeen <ajas.sharaf95@gmail.com>
2024-09-22 13:18:29 +0530

    product can now be added to cart from the pop up product page

--- client/src/components/shopping-view/product-details.jsx
+++ client/src/components/shopping-view/product-details.jsx
@@ -4,8 +4,28 @@ import { Button } from "../ui/button";
 import { Dialog, DialogContent } from "../ui/dialog";
 import { Separator } from "../ui/separator";
 import { Input } from "../ui/input";
+import { useDispatch, useSelector } from "react-redux";
+import { addToCart, fetchCartItems } from "@/store/shop/cart-slice";
+import { useToast } from "@/hooks/use-toast";
 
 export default function ProductDetailsDialog({ open, setOpen, productDetails }) {
+
+  const dispatch = useDispatch()
+  const { user } = useSelector(state => state.auth)
+  const { toast } = useToast()
+
+  function handleAddToCart(getCurrentProductId) {
+    dispatch(addToCart({ userId: user?.id, productId: getCurrentProductId, quantity: 1 }))
+      .then(data => {
+        if (data?.payload?.success) {
+          dispatch(fetchCartItems(user?.id))
+          toast({
+            title: 'Product is added to cart'
+          })
+        }
+      })
+  }
+
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogContent className='grid grid-cols-2 gap-8 sm:p-12 max-w-[90vw] sm:max-w-[80vw] lg:max-w-[70vw]'>
@@ -42,7 +62,7 @@ export default function ProductDetailsDialog({ open, setOpen, productDetails }) 
             <span className="text-muted-foreground">(4.5)</span>
           </div>
           <div className="mt-5 mb-5">
-            <Button className='w-full font-bold'>Add to Cart</Button>
+            <Button onClick={() => handleAddToCart(productDetails?._id)} className='w-full font-bold'>Add to Cart</Button>
           </div>
           <Separator />
           <div className="max-h-[300px] overflow-auto">
