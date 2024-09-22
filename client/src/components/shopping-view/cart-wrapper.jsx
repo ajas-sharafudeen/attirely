d6dbd49136d610a63b82aba070984dacb0b09a1a
Ajas Sharafudeen <ajas.sharaf95@gmail.com>
2024-09-22 13:17:44 +0530

    total cart amount is calculated and displayed

--- client/src/components/shopping-view/cart-wrapper.jsx
+++ client/src/components/shopping-view/cart-wrapper.jsx
@@ -3,6 +3,14 @@ import { SheetContent, SheetHeader, SheetTitle } from "../ui/sheet";
 import UserCartItemsContent from "./cart-items-content";
 
 export default function UserCartWrapper({ cartItems }) {
+
+  const totalCartAmount = cartItems && cartItems.length > 0 ?
+    cartItems.reduce((sum, currentItem) => sum + (
+      currentItem?.salePrice > 0 ?
+        currentItem?.salePrice :
+        currentItem?.price
+    ) * currentItem?.quantity, 0)
+    : 0
   return (
     <SheetContent className='sm:max-w-md'>
       <SheetHeader>
@@ -17,7 +25,7 @@ export default function UserCartWrapper({ cartItems }) {
       <div className="mt-8 space-y-4">
         <div className="flex justify-between">
           <span className="font-bold">Total</span>
-          <span className="font-bold">$990</span>
+          <span className="font-bold">{totalCartAmount}</span>
         </div>
       </div>
       <Button className='mt-6 w-full'>Checkout</Button>
