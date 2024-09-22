94437152982cb90f435f182c6df175a6f7e4e90b
Ajas Sharafudeen <ajas.sharaf95@gmail.com>
2024-09-22 13:19:20 +0530

    toast added for product added to cart

--- client/src/pages/shopping-view/listing.jsx
+++ client/src/pages/shopping-view/listing.jsx
@@ -10,6 +10,7 @@ import {
   DropdownMenuTrigger
 } from "@/components/ui/dropdown-menu";
 import { sortOptions } from "@/config";
+import { useToast } from "@/hooks/use-toast";
 import { addToCart, fetchCartItems } from "@/store/shop/cart-slice";
 import { fetchAllFilteredProducts, fetchProductDetails } from "@/store/shop/products-slice";
 import { ArrowUpDownIcon } from "lucide-react";
@@ -41,6 +42,7 @@ export default function ShoppingListing() {
   const [sort, setSort] = useState(null)
   const [searchParams, setSearchParams] = useSearchParams()
   const [openDetailsDialog, setOpenDetailsDialog] = useState(false)
+  const { toast } = useToast()
 
   function handleSort(value) {
     setSort(value)
@@ -75,6 +77,9 @@ export default function ShoppingListing() {
       .then(data => {
         if (data?.payload?.success) {
           dispatch(fetchCartItems(user?.id))
+          toast({
+            title: 'Product is added to cart'
+          })
         }
       })
   }
