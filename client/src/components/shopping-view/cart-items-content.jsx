3332c30ba91ddcd011f675d63d62009691be605e
Ajas Sharafudeen <ajas.sharaf95@gmail.com>
2024-09-22 13:17:10 +0530

    updating and deleting cart items completed

--- client/src/components/shopping-view/cart-items-content.jsx
+++ client/src/components/shopping-view/cart-items-content.jsx
@@ -1,19 +1,65 @@
 import { Minus, Plus, Trash } from "lucide-react";
 import { Button } from "../ui/button";
+import { useDispatch, useSelector } from 'react-redux';
+import { deleteCartItem, updateCartQuantity } from "@/store/shop/cart-slice";
+import { useToast } from "@/hooks/use-toast";
 
 export default function UserCartItemsContent({ cartItem }) {
+
+  const { user } = useSelector(state => state.auth)
+  const dispatch = useDispatch()
+  const { toast } = useToast()
+
+  function handleUpdateQuantity(getCartItem, typeOfAction) {
+    dispatch(updateCartQuantity({
+      userId: user?.id, productId: getCartItem?.productId, quantity:
+        typeOfAction === 'plus' ?
+          getCartItem?.quantity + 1 :
+          getCartItem?.quantity - 1
+    })).then(data => {
+      if (data?.payload?.success) {
+        toast({
+          title: 'Cart item is updated successfully'
+        })
+      }
+    })
+  }
+
+  function handleCartItemDelete(getCartItem) {
+    dispatch(deleteCartItem({
+      userId: user?.id, productId: getCartItem?.productId
+    })).then(data => {
+      if (data?.payload?.success) {
+        toast({
+          title: 'Cart item is deleted'
+        })
+      }
+    })
+  }
+
   return (
     <div className="flex items-center space-x-4">
       <img src={cartItem?.image} alt={cartItem?.title} className="w-20 h-20 rounded object-cover" />
       <div className="flex-1">
         <h3 className="font-extrabold">{cartItem?.title}</h3>
         <div className="flex items-center gap-2 mt-1">
-          <Button variant='outline' className='h-8 w-8 rounded-full' size='icon'>
+          <Button
+            variant='outline'
+            className='h-8 w-8 rounded-full'
+            size='icon'
+            disabled={cartItem?.quantity === 1}
+            onClick={() => handleUpdateQuantity(cartItem, 'minus')}
+          >
             <Minus className="w-4 h-4" />
             <span className="sr-only">Decrease</span>
           </Button>
-          <span className="font-semibold">cartItem?.quantity</span>
-          <Button variant='outline' className='h-8 w-8 rounded-full' size='icon'>
+          <span className="font-semibold">{cartItem?.quantity}</span>
+          <Button
+            variant='outline'
+            className='h-8 w-8 rounded-full'
+            size='icon'
+            onClick={() => handleUpdateQuantity(cartItem, 'plus')}
+          >
             <Plus className="w-4 h-4" />
             <span className="sr-only">Decrease</span>
           </Button>
@@ -23,7 +69,7 @@ export default function UserCartItemsContent({ cartItem }) {
         <p className="font-semibold">
           ${((cartItem?.salePrice > 0 ? cartItem?.salePrice : cartItem?.price) * cartItem?.quantity).toFixed(2)}
         </p>
-        <Trash className="cursor-pointer mt-1" size={20} />
+        <Trash onClick={() => handleCartItemDelete(cartItem)} className="cursor-pointer mt-1" size={20} />
       </div>
     </div>
   )
