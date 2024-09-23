480a552ec6b0486c84ffde50ee53eb4d4cfed786
Ajas Sharafudeen <ajas.sharaf95@gmail.com>
2024-09-23 22:51:42 +0530

    fixed formatting

--- client/src/pages/shopping-view/account.jsx
+++ client/src/pages/shopping-view/account.jsx
@@ -1,7 +1,7 @@
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import accountImg from "../../assets/account.jpg";
-import Orders from "@/components/shopping-view/orders";
-import Address from "@/components/shopping-view/address";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import accountImg from '../../assets/account.jpg';
+import Address from '@/components/shopping-view/address';
+import ShoppingOrders from '@/components/shopping-view/orders';
 
 export default function ShoppingAccount() {
   return (
@@ -14,20 +14,20 @@ export default function ShoppingAccount() {
       </div>
       <div className="container mx-auto grid grid-cols-1 gap-8 py-8">
         <div className="flex flex-col rounded-lg border bg-background p-6 shadow-sm">
-          <Tabs defaultValue='orders'>
+          <Tabs defaultValue="orders">
             <TabsList>
-              <TabsTrigger value='orders'>Orders</TabsTrigger>
-              <TabsTrigger value='address'>Address</TabsTrigger>
+              <TabsTrigger value="orders">Orders</TabsTrigger>
+              <TabsTrigger value="address">Address</TabsTrigger>
             </TabsList>
-            <TabsContent value='orders'>
-              <Orders />
+            <TabsContent value="orders">
+              <ShoppingOrders />
             </TabsContent>
-            <TabsContent value='address'>
+            <TabsContent value="address">
               <Address />
             </TabsContent>
           </Tabs>
         </div>
       </div>
     </div>
-  )
+  );
 }
