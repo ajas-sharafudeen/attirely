1723ecb6e79ff1c0eb0885db9afa82242067512d
Ajas Sharafudeen <ajas.sharaf95@gmail.com>
2024-09-23 22:47:41 +0530

    change font weight to labels, handles edit and delete address

--- client/src/components/shopping-view/address-card.jsx
+++ client/src/components/shopping-view/address-card.jsx
@@ -1,16 +1,25 @@
-import { Card, CardContent } from "../ui/card";
-import { Label } from "../ui/label";
+import { Button } from '../ui/button';
+import { Card, CardContent, CardFooter } from '../ui/card';
+import { Label } from '../ui/label';
 
-export default function AddressCard() {
+export default function AddressCard({
+  addressInfo,
+  handleEditAddress,
+  handleDeleteAddress,
+}) {
   return (
     <Card>
-      <CardContent className='grid gap-4'>
-        <Label>{addressInfo?.address}</Label>
-        <Label>{addressInfo?.city}</Label>
-        <Label>{addressInfo?.pincode}</Label>
-        <Label>{addressInfo?.phone}</Label>
-        <Label>{addressInfo?.notes}</Label>
+      <CardContent className="grid p-4 gap-4">
+        <Label className="font-bold">Address : {addressInfo?.address}</Label>
+        <Label className="font-bold">City : {addressInfo?.city}</Label>
+        <Label className="font-bold">Pincode : {addressInfo?.pincode}</Label>
+        <Label className="font-bold">Phone : {addressInfo?.phone}</Label>
+        <Label className="font-bold">Notes : {addressInfo?.notes}</Label>
       </CardContent>
+      <CardFooter className="flex p-3 justify-between">
+        <Button onClick={() => handleEditAddress(addressInfo)}>Edit</Button>
+        <Button onClick={() => handleDeleteAddress(addressInfo)}>Delete</Button>
+      </CardFooter>
     </Card>
-  )
+  );
 }
