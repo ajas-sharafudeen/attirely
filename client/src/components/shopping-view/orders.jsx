47b7d6014618d3f7e9ecc7b0eaa7a2148e354767
Ajas Sharafudeen <ajas.sharaf95@gmail.com>
2024-09-23 22:50:32 +0530

    orders component

--- client/src/components/shopping-view/orders.jsx
+++ client/src/components/shopping-view/orders.jsx
@@ -1,5 +1,58 @@
-export default function Orders() {
+import { useState } from 'react';
+import { Button } from '../ui/button';
+import { Card, CardContent, CardHeader, CardTitle } from '../ui/card';
+import { Dialog } from '../ui/dialog';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '../ui/table';
+import ShoppingOrderDetailsView from './order-details';
+
+export default function ShoppingOrders() {
+  const [openDetailsDialog, setOpenDetailsDialog] = useState(false);
   return (
-    <div>Orders</div>
-  )
+    <Card>
+      <CardHeader>
+        <CardTitle>Order History</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Order ID</TableHead>
+              <TableHead>Order Date</TableHead>
+              <TableHead>Order Status</TableHead>
+              <TableHead>Order Price</TableHead>
+              <TableHead>
+                <span className="sr-only">Details</span>
+              </TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell>123456</TableCell>
+              <TableCell>27/05/2022</TableCell>
+              <TableCell>PROCESSING</TableCell>
+              <TableCell>$1000</TableCell>
+              <TableCell>
+                <Dialog
+                  open={openDetailsDialog}
+                  onOpenChange={setOpenDetailsDialog}
+                >
+                  <Button onClick={() => setOpenDetailsDialog(true)}>
+                    View Details
+                  </Button>
+                  <ShoppingOrderDetailsView />
+                </Dialog>
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </CardContent>
+    </Card>
+  );
 }
