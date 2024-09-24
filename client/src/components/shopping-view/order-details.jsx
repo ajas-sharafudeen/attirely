da190fa0e5519126a94e486a316857ee53422f87
Ajas Sharafudeen <ajas.sharaf95@gmail.com>
2024-09-24 21:53:06 +0530

    order details

--- client/src/components/shopping-view/order-details.jsx
+++ client/src/components/shopping-view/order-details.jsx
@@ -1,27 +1,38 @@
+import { Badge } from '../ui/badge';
 import { DialogContent } from '../ui/dialog';
 import { Label } from '../ui/label';
 import { Separator } from '../ui/separator';
 
-export default function ShoppingOrderDetailsView() {
+export default function ShoppingOrderDetailsView({ orderDetails }) {
   return (
     <DialogContent className="sm:max-w-[600px]">
       <div className="grid gap-6">
         <div className="grid gap-2">
           <div className="flex mt-6 items-center justify-between">
             <p className="font-medium">Order ID</p>
-            <Label>1234</Label>
+            <Label>{orderDetails?._id}</Label>
           </div>
           <div className="flex mt-2 items-center justify-between">
             <p className="font-medium">Order Date</p>
-            <Label>20/11/2023</Label>
+            <Label>{orderDetails?.orderDate.split('T')[0]}</Label>
+          </div>
+          <div className="flex mt-2 items-center justify-between">
+            <p className="font-medium">Order Price</p>
+            <Label>${orderDetails?.totalAmount}</Label>
           </div>
           <div className="flex mt-2 items-center justify-between">
             <p className="font-medium">Order Status</p>
-            <Label>PROCESSING</Label>
-          </div>
-          <div className="flex mt-2 items-center justify-between">
-            <p className="font-medium">Price</p>
-            <Label>$500</Label>
+            <Label>
+              <Badge
+                className={`py-1 px-3 ${
+                  orderDetails?.orderStatus === 'confirmed'
+                    ? 'bg-green-600'
+                    : 'bg-orange-600'
+                }`}
+              >
+                {orderDetails?.orderStatus}
+              </Badge>
+            </Label>
           </div>
         </div>
         <Separator />
