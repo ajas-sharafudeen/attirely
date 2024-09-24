4a9d68ca7f54423292cfa14e4e234247e1ec8f22
Ajas Sharafudeen <ajas.sharaf95@gmail.com>
2024-09-24 21:46:18 +0530

    Route paths for paypal success and return pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,71 +1,86 @@
-import { Routes, Route } from 'react-router-dom'
-import AuthLayout from './components/auth/layout'
-import AuthLogin from './pages/auth/login'
-import AuthRegister from './pages/auth/register'
-import AdminLayout from './components/admin-view/layout'
-import AdminDashboard from './pages/admin-view/dashboard'
-import AdminFeatures from './pages/admin-view/features'
-import AdminOrders from './pages/admin-view/orders'
-import AdminProducts from './pages/admin-view/products'
-import NotFound from './pages/not-found'
-import ShoppingLayout from './components/shopping-view/layout'
-import ShoppingAccount from './pages/shopping-view/account'
-import ShoppingCheckout from './pages/shopping-view/checkout'
-import ShoppingHome from './pages/shopping-view/home'
-import ShoppingListing from './pages/shopping-view/listing'
-import CheckAuth from './components/common/check-auth'
-import UnauthPage from './pages/unauth-page'
-import { useDispatch, useSelector } from 'react-redux'
-import { checkAuth } from './store/auth-slice'
-import { useEffect } from 'react'
-import { Skeleton } from "@/components/ui/skeleton"
+import { Routes, Route } from 'react-router-dom';
+import AuthLayout from './components/auth/layout';
+import AuthLogin from './pages/auth/login';
+import AuthRegister from './pages/auth/register';
+import AdminLayout from './components/admin-view/layout';
+import AdminDashboard from './pages/admin-view/dashboard';
+import AdminFeatures from './pages/admin-view/features';
+import AdminOrders from './pages/admin-view/orders';
+import AdminProducts from './pages/admin-view/products';
+import NotFound from './pages/not-found';
+import ShoppingLayout from './components/shopping-view/layout';
+import ShoppingAccount from './pages/shopping-view/account';
+import ShoppingCheckout from './pages/shopping-view/checkout';
+import ShoppingHome from './pages/shopping-view/home';
+import ShoppingListing from './pages/shopping-view/listing';
+import CheckAuth from './components/common/check-auth';
+import UnauthPage from './pages/unauth-page';
+import { useDispatch, useSelector } from 'react-redux';
+import { checkAuth } from './store/auth-slice';
+import { useEffect } from 'react';
+import { Skeleton } from '@/components/ui/skeleton';
+import PaypalReturnPage from './pages/shopping-view/paypal-return';
+import PaymentSuccessPage from './pages/shopping-view/payment-success';
 
 function App() {
-  const { isAuthenticated, user, isLoading } = useSelector(state => state.auth)
-  const dispatch = useDispatch()
+  const { isAuthenticated, user, isLoading } = useSelector(
+    (state) => state.auth
+  );
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(checkAuth())
-  }, [dispatch])
+    dispatch(checkAuth());
+  }, [dispatch]);
 
-  if (isLoading) return <Skeleton className="w-[600px] h-[600px]" />
+  if (isLoading) return <Skeleton className="w-[600px] h-[600px]" />;
 
   return (
-    <div className='flex flex-col overflow-hidden bg-white'>
+    <div className="flex flex-col overflow-hidden bg-white">
       <Routes>
-        <Route path='/auth' element={
-          <CheckAuth isAuthenticated={isAuthenticated} user={user}>
-            <AuthLayout />
-          </CheckAuth>
-        }>
-          <Route path='login' element={<AuthLogin />} />
-          <Route path='register' element={<AuthRegister />} />
+        <Route
+          path="/auth"
+          element={
+            <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+              <AuthLayout />
+            </CheckAuth>
+          }
+        >
+          <Route path="login" element={<AuthLogin />} />
+          <Route path="register" element={<AuthRegister />} />
         </Route>
-        <Route path='/admin' element={
-          <CheckAuth isAuthenticated={isAuthenticated} user={user}>
-            <AdminLayout />
-          </CheckAuth>
-        }>
-          <Route path='dashboard' element={<AdminDashboard />} />
-          <Route path='features' element={<AdminFeatures />} />
-          <Route path='orders' element={<AdminOrders />} />
-          <Route path='products' element={<AdminProducts />} />
+        <Route
+          path="/admin"
+          element={
+            <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+              <AdminLayout />
+            </CheckAuth>
+          }
+        >
+          <Route path="dashboard" element={<AdminDashboard />} />
+          <Route path="features" element={<AdminFeatures />} />
+          <Route path="orders" element={<AdminOrders />} />
+          <Route path="products" element={<AdminProducts />} />
         </Route>
-        <Route path='/shop' element={
-          <CheckAuth isAuthenticated={isAuthenticated} user={user}>
-            <ShoppingLayout />
-          </CheckAuth>
-        }>
-          <Route path='account' element={<ShoppingAccount />} />
-          <Route path='checkout' element={<ShoppingCheckout />} />
-          <Route path='home' element={<ShoppingHome />} />
-          <Route path='listing' element={<ShoppingListing />} />
+        <Route
+          path="/shop"
+          element={
+            <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+              <ShoppingLayout />
+            </CheckAuth>
+          }
+        >
+          <Route path="account" element={<ShoppingAccount />} />
+          <Route path="checkout" element={<ShoppingCheckout />} />
+          <Route path="home" element={<ShoppingHome />} />
+          <Route path="listing" element={<ShoppingListing />} />
+          <Route path="paypal-return" element={<PaypalReturnPage />} />
+          <Route path="payment-success" element={<PaymentSuccessPage />} />
         </Route>
-        <Route path='/unauth-page' element={<UnauthPage />} />
-        <Route path='*' element={<NotFound />} />
+        <Route path="/unauth-page" element={<UnauthPage />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
