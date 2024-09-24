466a79ed4d5c082ac88e98a8bddb77f14e2e0c5f
Ajas Sharafudeen <ajas.sharaf95@gmail.com>
2024-09-24 21:48:54 +0530

    paypal payment initiate function

--- client/src/pages/shopping-view/checkout.jsx
+++ client/src/pages/shopping-view/checkout.jsx
@@ -1,11 +1,19 @@
 import Address from '@/components/shopping-view/address';
 import img from '../../assets/account.jpg';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import UserCartItemsContent from '@/components/shopping-view/cart-items-content';
 import { Button } from '@/components/ui/button';
+import { useState } from 'react';
+import { createNewOrder } from '@/store/shop/order-slice';
+import { toast } from '@/hooks/use-toast';
 
 export default function ShoppingCheckout() {
   const { cartItems } = useSelector((state) => state.shopCart);
+  const { user } = useSelector((state) => state.auth);
+  const { approvalURL } = useSelector((state) => state.shopOrder);
+  const [currentSelectedAddress, setCurrentSelectedAddress] = useState(null);
+  const [isPaymentStart, setIsPaymentStart] = useState(false);
+  const dispatch = useDispatch();
 
   const totalCartAmount =
     cartItems && cartItems.items && cartItems.items.length > 0
@@ -20,13 +28,75 @@ export default function ShoppingCheckout() {
         )
       : 0;
 
+  function handleInitiatePaypalPayment() {
+    if (cartItems.length === 0) {
+      toast({
+        title: 'Your cart is empty. Please add products to proceed',
+        variant: 'destructive',
+      });
+      return;
+    }
+
+    if (currentSelectedAddress === null) {
+      toast({
+        title: 'Please select one address to proceed.',
+        variant: 'destructive',
+      });
+      return;
+    }
+
+    const orderData = {
+      userId: user?.id,
+      cartId: cartItems?._id,
+      cartItems: cartItems.items.map((singleCartItem) => ({
+        productId: singleCartItem?.productId,
+        title: singleCartItem?.title,
+        image: singleCartItem?.image,
+        price:
+          singleCartItem?.salePrice > 0
+            ? singleCartItem?.salePrice
+            : singleCartItem?.price,
+        quantity: singleCartItem?.quantity,
+      })),
+      addressInfo: {
+        addressId: currentSelectedAddress?._id,
+        address: currentSelectedAddress?.address,
+        city: currentSelectedAddress?.city,
+        pincode: currentSelectedAddress?.pincode,
+        phone: currentSelectedAddress?.phone,
+        notes: currentSelectedAddress?.notes,
+      },
+      orderStatus: 'pending',
+      paymentMethod: 'paypal',
+      paymentStatus: 'pending',
+      totalAmount: totalCartAmount,
+      orderDate: new Date(),
+      orderUpdateDate: new Date(),
+      paymentId: '',
+      payerId: '',
+    };
+    console.log(orderData);
+
+    dispatch(createNewOrder(orderData)).then((data) => {
+      console.log(data, 'ajas');
+      if (data?.payload?.success) {
+        setIsPaymentStart(true);
+      } else {
+        setIsPaymentStart(false);
+      }
+    });
+  }
+
+  if (approvalURL) {
+    window.location.href = approvalURL;
+  }
   return (
     <div className="flex flex-col">
       <div className="relative h-[300px] w-full overflow-hidden">
         <img src={img} className="h-full w-full object-cover object-center" />
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-5 mt-5 p-5">
-        <Address />
+        <Address setCurrentSelectedAddress={setCurrentSelectedAddress} />
         <div className="flex flex-col gap-4">
           {cartItems && cartItems.items && cartItems.items.length > 0
             ? cartItems.items.map((item) => (
@@ -40,7 +110,12 @@ export default function ShoppingCheckout() {
             </div>
           </div>
           <div className="mt-4 w-full">
-            <Button className="w-full">Checkout with PayPal</Button>
+            <Button
+              onClick={() => handleInitiatePaypalPayment()}
+              className="w-full"
+            >
+              Checkout with PayPal
+            </Button>
           </div>
         </div>
       </div>
