9e63c255c6e9c0853eb668e74621cab6e30d7692
Ajas Sharafudeen <ajas.sharaf95@gmail.com>
2024-09-25 22:33:52 +0530

    formatted code

--- client/src/components/shopping-view/product-details.jsx
+++ client/src/components/shopping-view/product-details.jsx
@@ -1,39 +1,48 @@
-import { StarIcon } from "lucide-react";
-import { Avatar, AvatarFallback } from "../ui/avatar";
-import { Button } from "../ui/button";
-import { Dialog, DialogContent } from "../ui/dialog";
-import { Separator } from "../ui/separator";
-import { Input } from "../ui/input";
-import { useDispatch, useSelector } from "react-redux";
-import { addToCart, fetchCartItems } from "@/store/shop/cart-slice";
-import { useToast } from "@/hooks/use-toast";
+import { StarIcon } from 'lucide-react';
+import { Avatar, AvatarFallback } from '../ui/avatar';
+import { Button } from '../ui/button';
+import { Dialog, DialogContent } from '../ui/dialog';
+import { Separator } from '../ui/separator';
+import { Input } from '../ui/input';
+import { useDispatch, useSelector } from 'react-redux';
+import { addToCart, fetchCartItems } from '@/store/shop/cart-slice';
+import { setProductDetails } from '@/store/shop/products-slice';
+import { useToast } from '@/hooks/use-toast';
 
-export default function ProductDetailsDialog({ open, setOpen, productDetails }) {
-
-  const dispatch = useDispatch()
-  const { user } = useSelector(state => state.auth)
-  const { toast } = useToast()
+export default function ProductDetailsDialog({
+  open,
+  setOpen,
+  productDetails,
+}) {
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.auth);
+  const { toast } = useToast();
 
   function handleAddToCart(getCurrentProductId) {
-    dispatch(addToCart({ userId: user?.id, productId: getCurrentProductId, quantity: 1 }))
-      .then(data => {
-        if (data?.payload?.success) {
-          dispatch(fetchCartItems(user?.id))
-          toast({
-            title: 'Product is added to cart'
-          })
-        }
+    dispatch(
+      addToCart({
+        userId: user?.id,
+        productId: getCurrentProductId,
+        quantity: 1,
       })
+    ).then((data) => {
+      if (data?.payload?.success) {
+        dispatch(fetchCartItems(user?.id));
+        toast({
+          title: 'Product is added to cart',
+        });
+      }
+    });
   }
 
   function handleDialogClose() {
-    setOpen(false)
-    dispatch(setProductDetails())
+    setOpen(false);
+    dispatch(setProductDetails());
   }
 
   return (
     <Dialog open={open} onOpenChange={handleDialogClose}>
-      <DialogContent className='grid grid-cols-2 gap-8 sm:p-12 max-w-[90vw] sm:max-w-[80vw] lg:max-w-[70vw]'>
+      <DialogContent className="grid grid-cols-2 gap-8 sm:p-12 max-w-[90vw] sm:max-w-[80vw] lg:max-w-[70vw]">
         <div className="relative overflow-hidden rounded-lg">
           <img
             src={productDetails?.image}
@@ -46,15 +55,23 @@ export default function ProductDetailsDialog({ open, setOpen, productDetails }) 
         <div>
           <div>
             <h1 className="text-3xl font-extrabold">{productDetails?.title}</h1>
-            <p className="text-muted-foreground text-2xl mb-5 mt-4">{productDetails?.description}</p>
+            <p className="text-muted-foreground text-2xl mb-5 mt-4">
+              {productDetails?.description}
+            </p>
           </div>
           <div className="flex items-center justify-between">
-            <p className={`text-3xl font-bold text-primary ${productDetails?.salePrice > 0 ? 'line-through' : ''}`}>
+            <p
+              className={`text-3xl font-bold text-primary ${
+                productDetails?.salePrice > 0 ? 'line-through' : ''
+              }`}
+            >
               ${productDetails?.price}
             </p>
-            {
-              productDetails?.salePrice > 0 ? <p className="text-2xl font-bold font-muted-foreground">${productDetails?.salePrice}</p> : null
-            }
+            {productDetails?.salePrice > 0 ? (
+              <p className="text-2xl font-bold font-muted-foreground">
+                ${productDetails?.salePrice}
+              </p>
+            ) : null}
           </div>
           <div className="flex items-center gap-2">
             <div className="flex items-center gap-0 5">
@@ -67,14 +84,19 @@ export default function ProductDetailsDialog({ open, setOpen, productDetails }) 
             <span className="text-muted-foreground">(4.5)</span>
           </div>
           <div className="mt-5 mb-5">
-            <Button onClick={() => handleAddToCart(productDetails?._id)} className='w-full font-bold'>Add to Cart</Button>
+            <Button
+              onClick={() => handleAddToCart(productDetails?._id)}
+              className="w-full font-bold"
+            >
+              Add to Cart
+            </Button>
           </div>
           <Separator />
           <div className="max-h-[300px] overflow-auto">
             <h2 className="text-xl font-bold mb-4">Reviews</h2>
             <div className="grid gap-6">
               <div className="flex gap-4">
-                <Avatar className='w-10 h-10 border'>
+                <Avatar className="w-10 h-10 border">
                   <AvatarFallback>SN</AvatarFallback>
                 </Avatar>
                 <div className="grid gap-1">
@@ -95,12 +117,12 @@ export default function ProductDetailsDialog({ open, setOpen, productDetails }) 
               </div>
             </div>
             <div className="mt-6 flex gap-2">
-              <Input placeholder='Write a review...' />
+              <Input placeholder="Write a review..." />
               <Button>Submit</Button>
             </div>
           </div>
         </div>
       </DialogContent>
     </Dialog>
-  )
+  );
 }
