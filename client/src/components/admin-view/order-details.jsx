194590703c23ee8bae82bd22681fcfc21059b638
Ajas Sharafudeen <ajas.sharaf95@gmail.com>
2024-09-25 22:32:18 +0530

    replaced placeholders with api response data

--- client/src/components/admin-view/order-details.jsx
+++ client/src/components/admin-view/order-details.jsx
@@ -3,13 +3,16 @@ import CommonForm from '../common/form';
 import { DialogContent } from '../ui/dialog';
 import { Label } from '../ui/label';
 import { Separator } from '../ui/separator';
+import { Badge } from '../ui/badge';
+import { useSelector } from 'react-redux';
 
 const initialFormData = {
   status: '',
 };
 
-export default function AdminOrderDetailsView() {
+export default function AdminOrderDetailsView({ orderDetails }) {
   const [formData, setFormData] = useState(initialFormData);
+  const { user } = useSelector((state) => state.auth);
 
   function handleUpdateStatus(event) {
     event.preventDefault();
@@ -20,44 +23,67 @@ export default function AdminOrderDetailsView() {
       <div className="grid gap-6">
         <div className="grid gap-2">
           <div className="flex mt-6 items-center justify-between">
-            <p className="font-medium">Order ID</p>
-            <Label>1234</Label>
+            <p className="font-bold">Order ID</p>
+            <Label>{orderDetails?._id}</Label>
           </div>
           <div className="flex mt-2 items-center justify-between">
-            <p className="font-medium">Order Date</p>
-            <Label>20/11/2023</Label>
+            <p className="font-bold">Order Date</p>
+            <Label>{orderDetails?.orderDate.split('T')[0]}</Label>
           </div>
           <div className="flex mt-2 items-center justify-between">
-            <p className="font-medium">Order Status</p>
-            <Label>PROCESSING</Label>
+            <p className="font-bold">Order Price</p>
+            <Label>${orderDetails?.totalAmount}</Label>
           </div>
           <div className="flex mt-2 items-center justify-between">
-            <p className="font-medium">Price</p>
-            <Label>$500</Label>
+            <p className="font-bold">Payment Method</p>
+            <Label>{orderDetails?.paymentMethod}</Label>
+          </div>
+          <div className="flex mt-2 items-center justify-between">
+            <p className="font-bold">Payment Status</p>
+            <Label>{orderDetails?.paymentStatus}</Label>
+          </div>
+          <div className="flex mt-2 items-center justify-between">
+            <p className="font-bold">Order Status</p>
+            <Label>
+              <Badge
+                className={`py-1 px-3 ${
+                  orderDetails?.orderStatus === 'confirmed'
+                    ? 'bg-green-600'
+                    : 'bg-orange-600'
+                }`}
+              >
+                {orderDetails?.orderStatus}
+              </Badge>
+            </Label>
           </div>
         </div>
         <Separator />
         <div className="grid gap-4">
           <div className="grid gap-2">
-            <div className="font-medium">Order Details</div>
+            <div className="font-bold">Order Details</div>
             <ul className="grid gap-3">
-              <li className="flex items-center justify-between">
-                <span>Product One</span>
-                <span>$100</span>
-              </li>
+              {orderDetails?.cartItems && orderDetails?.cartItems.length > 0
+                ? orderDetails?.cartItems.map((item) => (
+                    <li className="flex items-center justify-between">
+                      <span>Title: {item.title}</span>
+                      <span>Quantity: {item.quantity}</span>
+                      <span>Price: $ {item.price}</span>
+                    </li>
+                  ))
+                : null}
             </ul>
           </div>
         </div>
         <div className="grid gap-4">
           <div className="grid gap-2">
-            <div className="font-medium">Shipping Information</div>
-            <div className="grid gap-0 5 text-muted-foregroud">
-              <span>Name</span>
-              <span>Address</span>
-              <span>City</span>
-              <span>Pincode</span>
-              <span>Phone</span>
-              <span>Notes</span>
+            <div className="font-bold">Shipping Information</div>
+            <div className="grid gap-0.5 text-muted-foreground">
+              <span>{user.userName}</span>
+              <span>{orderDetails?.addressInfo?.address}</span>
+              <span>{orderDetails?.addressInfo?.city}</span>
+              <span>{orderDetails?.addressInfo?.pincode}</span>
+              <span>{orderDetails?.addressInfo?.phone}</span>
+              <span>{orderDetails?.addressInfo?.notes}</span>
             </div>
           </div>
         </div>
