a590c31f48aa8d89798635b2fdd2d70ffa1cb05e
Ajas Sharafudeen <ajas.sharaf95@gmail.com>
2024-09-27 00:24:41 +0530

    stock limits and logic related to handling stocks across various components

--- client/src/components/shopping-view/cart-items-content.jsx
+++ client/src/components/shopping-view/cart-items-content.jsx
@@ -1,52 +1,97 @@
-import { Minus, Plus, Trash } from "lucide-react";
-import { Button } from "../ui/button";
+import { Minus, Plus, Trash } from 'lucide-react';
+import { Button } from '../ui/button';
 import { useDispatch, useSelector } from 'react-redux';
-import { deleteCartItem, updateCartQuantity } from "@/store/shop/cart-slice";
-import { useToast } from "@/hooks/use-toast";
+import { deleteCartItem, updateCartQuantity } from '@/store/shop/cart-slice';
+import { useToast } from '@/hooks/use-toast';
 
 export default function UserCartItemsContent({ cartItem }) {
-
-  const { user } = useSelector(state => state.auth)
-  const dispatch = useDispatch()
-  const { toast } = useToast()
+  const { user } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+  const { cartItems } = useSelector((state) => state.shopCart);
+  const { productList } = useSelector((state) => state.shopProducts);
+  const { toast } = useToast();
 
   function handleUpdateQuantity(getCartItem, typeOfAction) {
-    dispatch(updateCartQuantity({
-      userId: user?.id, productId: getCartItem?.productId, quantity:
-        typeOfAction === 'plus' ?
-          getCartItem?.quantity + 1 :
-          getCartItem?.quantity - 1
-    })).then(data => {
+    if (typeOfAction === 'plus') {
+      let getCartItems = cartItems.items || [];
+
+      if (getCartItems.length) {
+        const indexOfCurrentCartItem = getCartItems.findIndex(
+          (item) => item.productId === getCartItem.productId
+        );
+
+        const getCurrentProductIndex = productList.findIndex(
+          (product) => product._id === getCartItem?.productId
+        );
+
+        const getTotalStock = productList[getCurrentProductIndex].totalStock;
+        console.log(
+          getCurrentProductIndex,
+          getTotalStock,
+          'getCurrentProductIndex, getTotalStock'
+        );
+
+        if (indexOfCurrentCartItem > -1) {
+          const getQuantity = getCartItems[indexOfCurrentCartItem].quantity;
+          if (getQuantity + 1 > getTotalStock) {
+            toast({
+              title: `Only ${getQuantity} quantity can be added for this item`,
+              variant: 'destructive',
+            });
+
+            return;
+          }
+        }
+      }
+    }
+
+    dispatch(
+      updateCartQuantity({
+        userId: user?.id,
+        productId: getCartItem?.productId,
+        quantity:
+          typeOfAction === 'plus'
+            ? getCartItem?.quantity + 1
+            : getCartItem?.quantity - 1,
+      })
+    ).then((data) => {
       if (data?.payload?.success) {
         toast({
-          title: 'Cart item is updated successfully'
-        })
+          title: 'Cart item is updated successfully',
+        });
       }
-    })
+    });
   }
 
   function handleCartItemDelete(getCartItem) {
-    dispatch(deleteCartItem({
-      userId: user?.id, productId: getCartItem?.productId
-    })).then(data => {
+    dispatch(
+      deleteCartItem({
+        userId: user?.id,
+        productId: getCartItem?.productId,
+      })
+    ).then((data) => {
       if (data?.payload?.success) {
         toast({
-          title: 'Cart item is deleted'
-        })
+          title: 'Cart item is deleted',
+        });
       }
-    })
+    });
   }
 
   return (
     <div className="flex items-center space-x-4">
-      <img src={cartItem?.image} alt={cartItem?.title} className="w-20 h-20 rounded object-cover" />
+      <img
+        src={cartItem?.image}
+        alt={cartItem?.title}
+        className="w-20 h-20 rounded object-cover"
+      />
       <div className="flex-1">
         <h3 className="font-extrabold">{cartItem?.title}</h3>
         <div className="flex items-center gap-2 mt-1">
           <Button
-            variant='outline'
-            className='h-8 w-8 rounded-full'
-            size='icon'
+            variant="outline"
+            className="h-8 w-8 rounded-full"
+            size="icon"
             disabled={cartItem?.quantity === 1}
             onClick={() => handleUpdateQuantity(cartItem, 'minus')}
           >
@@ -55,9 +100,9 @@ export default function UserCartItemsContent({ cartItem }) {
           </Button>
           <span className="font-semibold">{cartItem?.quantity}</span>
           <Button
-            variant='outline'
-            className='h-8 w-8 rounded-full'
-            size='icon'
+            variant="outline"
+            className="h-8 w-8 rounded-full"
+            size="icon"
             onClick={() => handleUpdateQuantity(cartItem, 'plus')}
           >
             <Plus className="w-4 h-4" />
@@ -67,10 +112,18 @@ export default function UserCartItemsContent({ cartItem }) {
       </div>
       <div className="flex flex-col items-end">
         <p className="font-semibold">
-          ${((cartItem?.salePrice > 0 ? cartItem?.salePrice : cartItem?.price) * cartItem?.quantity).toFixed(2)}
+          $
+          {(
+            (cartItem?.salePrice > 0 ? cartItem?.salePrice : cartItem?.price) *
+            cartItem?.quantity
+          ).toFixed(2)}
         </p>
-        <Trash onClick={() => handleCartItemDelete(cartItem)} className="cursor-pointer mt-1" size={20} />
+        <Trash
+          onClick={() => handleCartItemDelete(cartItem)}
+          className="cursor-pointer mt-1"
+          size={20}
+        />
       </div>
     </div>
-  )
+  );
 }
