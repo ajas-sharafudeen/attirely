dd3c28c28852a72c2f7bbde996d28a69b806a79f
Ajas Sharafudeen <ajas.sharaf95@gmail.com>
2024-09-27 20:28:51 +0530

    formatted with prettier

--- client/src/components/admin-view/product-tile.jsx
+++ client/src/components/admin-view/product-tile.jsx
@@ -1,12 +1,12 @@
-import { Button } from "../ui/button";
-import { Card, CardContent, CardFooter } from "../ui/card";
+import { Button } from '../ui/button';
+import { Card, CardContent, CardFooter } from '../ui/card';
 
 export default function AdminProductTile({
   product,
   setFormData,
   setOpenCreateProductsDialog,
   setCurrentEditedId,
-  handleDelete
+  handleDelete,
 }) {
   return (
     <Card className="w-full max-w-sm mx-auto">
@@ -22,8 +22,9 @@ export default function AdminProductTile({
           <h2 className="text-xl font-bold mb-2 mt-2">{product?.title}</h2>
           <div className="flex justify-between items-center mb-2">
             <span
-              className={`${product?.salePrice > 0 ? "line-through" : ""
-                } text-lg font-semibold text-primary`}
+              className={`${
+                product?.salePrice > 0 ? 'line-through' : ''
+              } text-lg font-semibold text-primary`}
             >
               ${product?.price}
             </span>
