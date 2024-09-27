7d77f9891799ebeea18d80ee795e900462c43c01
Ajas Sharafudeen <ajas.sharaf95@gmail.com>
2024-09-27 20:27:03 +0530

    formatted with prettier

--- client/src/components/admin-view/header.jsx
+++ client/src/components/admin-view/header.jsx
@@ -1,28 +1,30 @@
-import { AlignJustify, LogOut } from "lucide-react";
-import { Button } from "../ui/button";
-import { logoutUser } from "@/store/auth-slice";
-import { useDispatch } from "react-redux";
+import { AlignJustify, LogOut } from 'lucide-react';
+import { Button } from '../ui/button';
+import { logoutUser } from '@/store/auth-slice';
+import { useDispatch } from 'react-redux';
 
 export default function AdminHeader({ setOpen }) {
-
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   function handleLogout() {
-    dispatch(logoutUser())
+    dispatch(logoutUser());
   }
 
   return (
     <header className="flex items-center justify-between px-4 py-3 bg-background border-b">
-      <Button onClick={() => setOpen(true)} className='lg:hidden sm:block'>
+      <Button onClick={() => setOpen(true)} className="lg:hidden sm:block">
         <AlignJustify />
         <span className="sr-only">Toggle Menu</span>
       </Button>
       <div className="flex flex-1 justify-end">
-        <Button onClick={handleLogout} className='inline-flex gap-2 items-center rounded-md px-4 py-2 text-sm font-bold shadow'>
+        <Button
+          onClick={handleLogout}
+          className="inline-flex gap-2 items-center rounded-md px-4 py-2 text-sm font-bold shadow"
+        >
           <LogOut />
           Log out
         </Button>
       </div>
     </header>
-  )
-};
+  );
+}
