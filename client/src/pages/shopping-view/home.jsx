b840b59d2d81b391cc2a37a921c10b2be65926a6
Ajas Sharafudeen <ajas.sharaf95@gmail.com>
2024-09-27 18:08:04 +0530

    gets featured images upon page load

--- client/src/pages/shopping-view/home.jsx
+++ client/src/pages/shopping-view/home.jsx
@@ -3,13 +3,18 @@ import bannerOne from '../../assets/banner-1.webp';
 import bannerTwo from '../../assets/banner-2.webp';
 import bannerThree from '../../assets/banner-3.webp';
 import {
+  Airplay,
   BabyIcon,
   ChevronLeftIcon,
   ChevronRightIcon,
   CloudLightning,
-  Dice1,
+  Heater,
+  Images,
+  Shirt,
   ShirtIcon,
-  Umbrella,
+  ShoppingBasket,
+  UmbrellaIcon,
+  WashingMachine,
   WatchIcon,
 } from 'lucide-react';
 import { Card, CardContent } from '@/components/ui/card';
@@ -22,31 +27,33 @@ import {
 import ShoppingProductTile from '@/components/shopping-view/product-tile';
 import { useNavigate } from 'react-router-dom';
 import { addToCart, fetchCartItems } from '@/store/shop/cart-slice';
-import { useToast } from '@/hooks/use-toast';
+import { useToast } from '@/components/ui/use-toast';
 import ProductDetailsDialog from '@/components/shopping-view/product-details';
+import { getFeatureImages } from '@/store/common-slice';
 
 const categoriesWithIcon = [
   { id: 'men', label: 'Men', icon: ShirtIcon },
   { id: 'women', label: 'Women', icon: CloudLightning },
   { id: 'kids', label: 'Kids', icon: BabyIcon },
   { id: 'accessories', label: 'Accessories', icon: WatchIcon },
-  { id: 'footwear', label: 'Footwear', icon: Umbrella },
+  { id: 'footwear', label: 'Footwear', icon: UmbrellaIcon },
 ];
 
 const brandsWithIcon = [
-  { id: 'nike', label: 'Nike', icon: Dice1 },
-  { id: 'adidas', label: 'Adidas', icon: Dice1 },
-  { id: 'puma', label: 'Puma', icon: Dice1 },
-  { id: 'levi', label: "Levi's", icon: Dice1 },
-  { id: 'zara', label: 'Zara', icon: Dice1 },
-  { id: 'h&m', label: 'H&M', icon: Dice1 },
+  { id: 'nike', label: 'Nike', icon: Shirt },
+  { id: 'adidas', label: 'Adidas', icon: WashingMachine },
+  { id: 'puma', label: 'Puma', icon: ShoppingBasket },
+  { id: 'levi', label: "Levi's", icon: Airplay },
+  { id: 'zara', label: 'Zara', icon: Images },
+  { id: 'h&m', label: 'H&M', icon: Heater },
 ];
-
 export default function ShoppingHome() {
   const [currentSlide, setCurrentSlide] = useState(0);
   const { productList, productDetails } = useSelector(
     (state) => state.shopProducts
   );
+  const { featureImageList } = useSelector((state) => state.commonFeature);
+
   const [openDetailsDialog, setOpenDetailsDialog] = useState(false);
 
   const { user } = useSelector((state) => state.auth);
@@ -55,13 +62,12 @@ export default function ShoppingHome() {
   const navigate = useNavigate();
   const { toast } = useToast();
 
-  const slides = [bannerOne, bannerTwo, bannerThree];
-
   function handleNavigateToListingPage(getCurrentItem, section) {
     sessionStorage.removeItem('filters');
     const currentFilter = {
       [section]: [getCurrentItem.id],
     };
+
     sessionStorage.setItem('filters', JSON.stringify(currentFilter));
     navigate(`/shop/listing`);
   }
@@ -70,7 +76,7 @@ export default function ShoppingHome() {
     dispatch(fetchProductDetails(getCurrentProductId));
   }
 
-  function handleAddToCart(getCurrentProductId) {
+  function handleAddtoCart(getCurrentProductId) {
     dispatch(
       addToCart({
         userId: user?.id,
@@ -93,10 +99,11 @@ export default function ShoppingHome() {
 
   useEffect(() => {
     const timer = setInterval(() => {
-      setCurrentSlide((prevSlide) => (prevSlide + 1) % slides.length);
-    }, 5000);
+      setCurrentSlide((prevSlide) => (prevSlide + 1) % featureImageList.length);
+    }, 10000);
+
     return () => clearInterval(timer);
-  }, []);
+  }, [featureImageList]);
 
   useEffect(() => {
     dispatch(
@@ -107,37 +114,49 @@ export default function ShoppingHome() {
     );
   }, [dispatch]);
 
+  console.log(productList, 'productList');
+
+  useEffect(() => {
+    dispatch(getFeatureImages());
+  }, [dispatch]);
+
   return (
     <div className="flex flex-col min-h-screen">
       <div className="relative w-full h-[600px] overflow-hidden">
-        {slides.map((slide, index) => (
-          <img
-            src={slide}
-            key={index}
-            className={`${
-              index === currentSlide ? 'opacity-100' : 'opacity-0'
-            } absolute top-0 left-0 w-full h-full object-cover transition-opacity duration-1000`}
-          />
-        ))}
+        {featureImageList && featureImageList.length > 0
+          ? featureImageList.map((slide, index) => (
+              <img
+                src={slide?.image}
+                key={index}
+                className={`${
+                  index === currentSlide ? 'opacity-100' : 'opacity-0'
+                } absolute top-0 left-0 w-full h-full object-cover transition-opacity duration-1000`}
+              />
+            ))
+          : null}
         <Button
+          variant="outline"
+          size="icon"
           onClick={() =>
             setCurrentSlide(
-              (prevSlide) => (prevSlide - 1 + slides.length) % slides.length
+              (prevSlide) =>
+                (prevSlide - 1 + featureImageList.length) %
+                featureImageList.length
             )
           }
           className="absolute top-1/2 left-4 transform -translate-y-1/2 bg-white/80"
-          variant="outline"
-          size="icon"
         >
           <ChevronLeftIcon className="w-4 h-4" />
         </Button>
         <Button
-          onClick={() =>
-            setCurrentSlide((prevSlide) => (prevSlide + 1) % slides.length)
-          }
-          className="absolute top-1/2 right-4 transform -translate-y-1/2 bg-white/80"
           variant="outline"
           size="icon"
+          onClick={() =>
+            setCurrentSlide(
+              (prevSlide) => (prevSlide + 1) % featureImageList.length
+            )
+          }
+          className="absolute top-1/2 right-4 transform -translate-y-1/2 bg-white/80"
         >
           <ChevronRightIcon className="w-4 h-4" />
         </Button>
@@ -147,7 +166,7 @@ export default function ShoppingHome() {
           <h2 className="text-3xl font-bold text-center mb-8">
             Shop by category
           </h2>
-          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-4">
+          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
             {categoriesWithIcon.map((categoryItem) => (
               <Card
                 onClick={() =>
@@ -164,6 +183,7 @@ export default function ShoppingHome() {
           </div>
         </div>
       </section>
+
       <section className="py-12 bg-gray-50">
         <div className="container mx-auto px-4">
           <h2 className="text-3xl font-bold text-center mb-8">Shop by Brand</h2>
@@ -182,10 +202,11 @@ export default function ShoppingHome() {
           </div>
         </div>
       </section>
+
       <section className="py-12">
         <div className="container mx-auto px-4">
           <h2 className="text-3xl font-bold text-center mb-8">
-            Featured Products
+            Feature Products
           </h2>
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
             {productList && productList.length > 0
@@ -193,7 +214,7 @@ export default function ShoppingHome() {
                   <ShoppingProductTile
                     handleGetProductDetails={handleGetProductDetails}
                     product={productItem}
-                    handleAddToCart={handleAddToCart}
+                    handleAddtoCart={handleAddtoCart}
                   />
                 ))
               : null}
