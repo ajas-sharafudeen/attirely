a19f7eb1d39772b6dc43f481e00adc9a0b33b964
Ajas Sharafudeen <ajas.sharaf95@gmail.com>
2024-09-27 18:12:51 +0530

    store integrated with review and common slices

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -8,17 +8,24 @@ import shopCartSlice from './shop/cart-slice';
 import shopAddressSlice from './shop/address-slice';
 import shopOrderSlice from './shop/order-slice';
 import shopSearchSlice from './shop/search-slice';
+import shopReviewSlice from './shop/review-slice';
+import commonFeatureSlice from './common-slice';
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
+
     adminProducts: adminProductsSlice,
     adminOrder: adminOrderSlice,
+
     shopProducts: shopProductsSlice,
     shopCart: shopCartSlice,
     shopAddress: shopAddressSlice,
     shopOrder: shopOrderSlice,
     shopSearch: shopSearchSlice,
+    shopReview: shopReviewSlice,
+
+    commonFeature: commonFeatureSlice,
   },
 });
 
