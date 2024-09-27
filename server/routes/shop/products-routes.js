9a5075ce8ee446ab7304c3982147e3bbccf33c3b
Ajas Sharafudeen <ajas.sharaf95@gmail.com>
2024-09-27 20:37:51 +0530

    formatted with prettier

--- server/routes/shop/products-routes.js
+++ server/routes/shop/products-routes.js
@@ -1,13 +1,13 @@
-const express = require("express");
+const express = require('express');
 
 const {
   getFilteredProducts,
   getProductDetails,
-} = require("../../controllers/shop/products-controller");
+} = require('../../controllers/shop/products-controller');
 
 const router = express.Router();
 
-router.get("/get", getFilteredProducts);
-router.get("/get/:id", getProductDetails);
+router.get('/get', getFilteredProducts);
+router.get('/get/:id', getProductDetails);
 
 module.exports = router;
